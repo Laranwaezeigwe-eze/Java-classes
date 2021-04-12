912ec5813d47033050d74a23a607bb4668f3a66c
Omolara Nwaezeigwe-eze <davinel.davinel@gmail.com>
2021-04-12 16:32:17 +0100

    exercise

---
+++ src/chapterTwo/TableOfSquaresAndCubes.java
@@ -0,0 +1,55 @@
+package chapterTwo;
+
+public class TableOfSquaresAndCubes {
+    public static void main(String[] args) {
+        System.out.printf("%-10s%-10s%-10s%n", "number","square","cube");
+        int number = 0;
+        int square = number * number;
+        int cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 1;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 2;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 3;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 4;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 5;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 6;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 7;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 8 ;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 9 ;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+        number = 10 ;
+        square = number * number;
+        cube = number * number * number;
+        System.out.printf("%-10s%-10s%-10s%n",number,square,cube);
+
+
+
+
+    }
+}
